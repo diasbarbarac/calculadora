e2c747f2a74cd2bdb63cb2be7b972a13846cbd9c
Bárbara Dias <110254970+diasbarbarac@users.noreply.github.com>
2022-08-10 06:56:47 -0300

    Add files via upload

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace WindowsFormsApp5calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnCalc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtNum1 = new System.Windows.Forms.TextBox();
+            this.CbotipoCalc = new System.Windows.Forms.ComboBox();
+            this.TxtNum2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnLimpar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Lplresultado = new System.Windows.Forms.Label();
+            this.LblResultado = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BtnSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(130, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 152);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnCalc
+            // 
+            this.BtnCalc.BackColor = System.Drawing.Color.LightBlue;
+            this.BtnCalc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCalc.Location = new System.Drawing.Point(61, 14);
+            this.BtnCalc.Name = "BtnCalc";
+            this.BtnCalc.Size = new System.Drawing.Size(100, 36);
+            this.BtnCalc.TabIndex = 1;
+            this.BtnCalc.Text = "Calcular";
+            this.BtnCalc.UseVisualStyleBackColor = false;
+            this.BtnCalc.Click += new System.EventHandler(this.BtnCalc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(109, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Número1";
+            // 
+            // TxtNum1
+            // 
+            this.TxtNum1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtNum1.Location = new System.Drawing.Point(165, 207);
+            this.TxtNum1.Name = "TxtNum1";
+            this.TxtNum1.Size = new System.Drawing.Size(119, 20);
+            this.TxtNum1.TabIndex = 3;
+            this.TxtNum1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // CbotipoCalc
+            // 
+            this.CbotipoCalc.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CbotipoCalc.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.CbotipoCalc.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CbotipoCalc.FormattingEnabled = true;
+            this.CbotipoCalc.Items.AddRange(new object[] {
+            "Divisão",
+            "Multiplicação",
+            "Subtração",
+            "Adição"});
+            this.CbotipoCalc.Location = new System.Drawing.Point(165, 259);
+            this.CbotipoCalc.Name = "CbotipoCalc";
+            this.CbotipoCalc.Size = new System.Drawing.Size(121, 21);
+            this.CbotipoCalc.TabIndex = 6;
+            // 
+            // TxtNum2
+            // 
+            this.TxtNum2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtNum2.Location = new System.Drawing.Point(167, 233);
+            this.TxtNum2.Name = "TxtNum2";
+            this.TxtNum2.Size = new System.Drawing.Size(119, 20);
+            this.TxtNum2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(109, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Número2";
+            // 
+            // BtnLimpar
+            // 
+            this.BtnLimpar.BackColor = System.Drawing.Color.LightBlue;
+            this.BtnLimpar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLimpar.Location = new System.Drawing.Point(220, 14);
+            this.BtnLimpar.Name = "BtnLimpar";
+            this.BtnLimpar.Size = new System.Drawing.Size(100, 36);
+            this.BtnLimpar.TabIndex = 9;
+            this.BtnLimpar.Text = "Limpar";
+            this.BtnLimpar.UseVisualStyleBackColor = false;
+            this.BtnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(88, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Tipo Cálculo";
+            // 
+            // Lplresultado
+            // 
+            this.Lplresultado.AutoSize = true;
+            this.Lplresultado.Location = new System.Drawing.Point(164, 300);
+            this.Lplresultado.Name = "Lplresultado";
+            this.Lplresultado.Size = new System.Drawing.Size(0, 13);
+            this.Lplresultado.TabIndex = 11;
+            // 
+            // LblResultado
+            // 
+            this.LblResultado.AutoSize = true;
+            this.LblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblResultado.ForeColor = System.Drawing.Color.SteelBlue;
+            this.LblResultado.Location = new System.Drawing.Point(170, 310);
+            this.LblResultado.Name = "LblResultado";
+            this.LblResultado.Size = new System.Drawing.Size(92, 18);
+            this.LblResultado.TabIndex = 12;
+            this.LblResultado.Text = "Resultado =";
+            this.LblResultado.Click += new System.EventHandler(this.LblResultado_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.SlateBlue;
+            this.panel1.Controls.Add(this.BtnLimpar);
+            this.panel1.Controls.Add(this.BtnCalc);
+            this.panel1.Location = new System.Drawing.Point(0, 356);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(439, 55);
+            this.panel1.TabIndex = 13;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.SlateBlue;
+            this.panel2.Controls.Add(this.BtnSair);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(0, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(439, 31);
+            this.panel2.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(149, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Calculadora";
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.BackColor = System.Drawing.Color.Red;
+            this.BtnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSair.Location = new System.Drawing.Point(372, 3);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(54, 28);
+            this.BtnSair.TabIndex = 10;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = false;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(438, 414);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.LblResultado);
+            this.Controls.Add(this.Lplresultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtNum2);
+            this.Controls.Add(this.CbotipoCalc);
+            this.Controls.Add(this.TxtNum1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BtnLimpar";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BtnCalc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtNum1;
+        private System.Windows.Forms.ComboBox CbotipoCalc;
+        private System.Windows.Forms.TextBox TxtNum2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnLimpar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Lplresultado;
+        private System.Windows.Forms.Label LblResultado;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button BtnSair;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblResultado_Click(object sender, EventArgs e)
+        {
+            LblResultado.Text= " ";
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            DialogResult Sair;
+            Sair = MessageBox.Show("Deseja mesmo sair do sistema ?", "Pergunta", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (Sair.Equals(DialogResult.No))
+            {
+
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void BtnCalc_Click(object sender, EventArgs e)
+        {
+            double n1 = 0;
+            double n2 = 0;
+
+            double resultado = 0;
+
+            n1= Convert.ToDouble(TxtNum1.Text);
+            n2=Convert.ToDouble(TxtNum2.Text);
+
+            switch (CbotipoCalc.SelectedItem.ToString())
+            {
+                case "Divisão":
+                    resultado= n1/n2;
+                    break;
+
+                case "Multiplicação":
+                    resultado= n1*n2;
+                    break;
+
+                case "Adição":
+                    resultado= n1+n2;
+                    break;
+
+                case "Subtração":
+                    resultado= n1-n2;
+                    break;
+            }
+            
+            
+            
+            
+            
+            LblResultado.Text= Convert.ToString(resultado);
+           
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            TxtNum1.Clear();
+            TxtNum2.Clear();
+            LblResultado.Text= "";
+        }
+    }
+}
